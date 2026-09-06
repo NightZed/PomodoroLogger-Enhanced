08313f33e9ecbd0fd01d2a9195cbb8496416c02d
NightZed <Night.Zed@outlook.com>
2026-09-07 00:15:21 +0800

    fix: 修复开发者控制台的30s超时错误

--- src/renderer/workers/db.worker.ts
+++ src/renderer/workers/db.worker.ts
@@ -8,7 +8,7 @@ export async function loadDB(path: string): Promise<nedb> {
     return await new Promise((resolve, reject) => {
         let times = 0;
         const load = () => {
-            db.loadDatabase(err => {
+            db.loadDatabase((err) => {
                 if (!err) {
                     resolve(db);
                     return;
@@ -44,7 +44,7 @@ function genHandleFunc(opName: string, leastArgNum: number = 0) {
         args.push((err: Error, doc: any) => {
             done({
                 type: 'done',
-                payload: doc
+                payload: doc,
             });
         });
 
@@ -58,5 +58,6 @@ addWorkerListeners(ctx, {
     findOne: genHandleFunc('findOne', 1),
     update: genHandleFunc('update', 2),
     insert: genHandleFunc('insert', 1),
-    remove: genHandleFunc('remove', 1)
+    remove: genHandleFunc('remove', 1),
+    count: genHandleFunc('count', 1),
 });
